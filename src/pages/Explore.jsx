3feb7961bcab72a29cfbc420a24a1a2734e519d1
ayushbhusal00 <ayushbhusal00@gmail.com>
2023-07-22 18:29:04 +0545

    Completed basic filters for explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,116 +1,107 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
-import { getAllWines } from "../utils/data.utils";
+import React, { useEffect, useState } from "react";
+import { filterWines, getAllWines } from "../utils/data.utils";
 import WineCardVertical from "../components/WineCardVertical";
-import PriceRangeSlider from "../components/PriceRangeSlider";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { useEffect } from "react";
 
 export default function Explore() {
-  const wineTypes = [
-    "Red",
-    "White",
-    "Sparkling",
-    "Rosé",
-    "Dessert",
-    "Fortified",
-  ];
-  const grapes = [
-    "Cabernet Sauvignon",
-    "Cabernet Franc",
-    "Chardonnay",
-    "Grenache",
-    "Malbec",
-    "Merlot",
-    "Pinot Noir",
-    "Riesling",
-    "Sauvignon Blanc",
-    "Shiraz/Syrah",
-  ];
+  //Changes in state for this components
+  const [wineList, setWineList] = useState([]);
+  //Values for generating FilteredWine List
   const [selectedTypes, setSelectedTypes] = useState([]);
   const [selectedGrapes, setSelectedGrapes] = useState([]);
+  //Values that make the sidebar categories for Types and Grapes dynamic
+  const [wineTypes, setWineTypes] = useState([]);
+  const [wineGrapes, setWineGrapes] = useState([]);
+  //Booleans to hide or show all sidebar category elements
+  const [showMoreTypes, setShowMoreTypes] = useState(false);
+  const [showMoreGrapes, setShowMoreGrapes] = useState(false);
+
+  //This function updates the Selected Wine Types Values
   const handleTypeClick = (type) => {
     if (selectedTypes.includes(type)) {
-      setSelectedTypes(
-        selectedTypes.filter((selectedType) => selectedType !== type)
+      setSelectedTypes((prevSelectedTypes) =>
+        prevSelectedTypes.filter((selectedType) => selectedType !== type)
       );
     } else {
       setSelectedTypes([...selectedTypes, type]);
     }
   };
-  const removeTypeClick = (type) => {
-    if (selectedTypes.includes(type)) {
-      setSelectedTypes(
-        selectedTypes.filter((selectedType) => selectedType !== type)
-      );
-    }
-  };
 
-  const removeGrapeClick = (type) => {
-    if (selectedGrapes.includes(type)) {
-      setSelectedGrapes(
-        selectedGrapes.filter((selectedGrape) => selectedGrape !== type)
-      );
-    }
-  };
+  //This function updates the Selected Wine Types Values
   const handleGrapeClick = (type) => {
     if (selectedGrapes.includes(type)) {
-      setSelectedGrapes(
-        selectedGrapes.filter((selectedGrape) => selectedGrape !== type)
+      setSelectedGrapes((prevSelectedGrapes) =>
+        prevSelectedGrapes.filter((selectedGrape) => selectedGrape !== type)
       );
     } else {
       setSelectedGrapes([...selectedGrapes, type]);
     }
   };
+
+  //This returns a boolean to identify if the types have selected CSS
   const isTypeSelected = (type) => {
     return selectedTypes.includes(type);
   };
-  const isGrapeSelected = (type) => {
-    return selectedGrapes.includes(type);
+
+  //This returns a boolean to identify if the types have selected CSS
+  const isGrapeSelected = (grape) => {
+    return selectedGrapes.includes(grape);
   };
-  // const location = useLocation();
-  // const searchParams = new URLSearchParams(location.search);
-  // const category = searchParams.get("filter");
-  const { category } = useParams();
-  // const filteredData = data.filter((item) => item.category === category);
-  // const filteredData = getWineList(category);
-  const [wineList, setWineList] = useState([]);
 
   useEffect(() => {
-    setWineList(getAllWines(category));
-  }, []);
+    // Fetch filtered wines when the component is first rendered or when selectedTypes or selectedGrapes change
+    const updateWineList = async () => {
+      // Use the filterWines function to get the filtered wine list based on selectedTypes and selectedGrapes
+      if (selectedTypes.length === 0 && selectedGrapes.length === 0) {
+        // Fetch all wines if no types and grapes are selected
+        setWineList(await getAllWines());
+      } else {
+        // console.log(selectedTypes, selectedGrapes);
+        setWineList(await filterWines(selectedTypes, selectedGrapes));
+      }
+    };
+    updateWineList();
+  }, [selectedTypes, selectedGrapes]);
+
+  // Create a separate useEffect for updating WineGrapes
+  useEffect(() => {
+    // Update the wineTypes state
+    const allGrapeTypes = new Set();
+    getAllWines()
+      .flat()
+      .forEach((item) => {
+        item.grape.forEach((grape) => {
+          allGrapeTypes.add(grape);
+        });
+      });
+    setWineGrapes(Array.from(allGrapeTypes).sort());
+  }, [wineList]); // Include wineList as a dependency for this useEffect
+
+  // Create a separate useEffect for updating WineTypes
+  useEffect(() => {
+    // Update the wineTypes state
+    const allWineTypes = new Set();
+    getAllWines().forEach((item) => allWineTypes.add(item.category));
+    setWineTypes(Array.from(allWineTypes).sort());
+  }, [wineList]); // Include wineList as a dependency for this useEffect
+
+  //Toggles showMoreTypes variable when button is clicked
+  const handleShowMoreTypesClick = () => {
+    setShowMoreTypes(!showMoreTypes);
+  };
+  const handleShowMoreGrapesClick = () => {
+    setShowMoreGrapes(!showMoreGrapes);
+  };
 
   return (
     <div className="container flex flex-col mobile:p-8">
       <div className="font-bold text-lg text-slate-900 mb-8">
-        {wineList.length === 0 ? (
-          <p className="p-6 text-base bg-slate-100 rounded-lg text-slate-400">
-            There are no wine list in this category
-          </p>
-        ) : (
-          <h1 className="font-bold text-lg text-slate-900 mb-8">{`Showing ${
-            wineList.length
-          } ${
-            category !== null && category !== undefined
-              ? category + " wines"
-              : "Wines"
-          } `}</h1>
-        )}
         <div className="text-base font-medium">
-          <p className="mb-4">From 1 grape and 1 regional wine style</p>{" "}
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              console.log("Button Clicked");
-            }}
-          >
-            Click to Update
-          </button>
           <div className="flex flex-row flex-wrap gap-2">
             {selectedTypes.map((type) => {
               return (
                 <button
-                  onClick={() => removeTypeClick(type)}
+                  onClick={() => handleTypeClick(type)}
                   className={`px-4 py-2 flex gap-2 items-center rounded-lg text-sm border-[1px] border-red-700 bg-red-700 text-white hover:bg-red-800 active:bg-red-900`}
                   key={type}
                 >
@@ -122,7 +113,7 @@ export default function Explore() {
             {selectedGrapes.map((grape) => {
               return (
                 <button
-                  onClick={() => removeGrapeClick(grape)}
+                  onClick={() => handleGrapeClick(grape)}
                   className={`px-4 py-2 flex gap-2 items-center rounded-lg text-sm border-[1px] border-red-700 bg-red-700 text-white hover:bg-red-800 active:bg-red-900`}
                   key={grape}
                 >
@@ -136,53 +127,122 @@ export default function Explore() {
       </div>
       <div className="grid grid-cols-3 gap-4">
         <div className="col-span-1 flex flex-col gap-8">
-          <div>
-            <h3 className="font-bold text-base mb-6">Wine Types</h3>
+          {wineTypes.length ? (
+            <div>
+              <h3 className="font-bold text-base mb-6">Wine Types</h3>
 
-            <div className="flex flex-wrap gap-2">
-              {wineTypes.map((type, index) => (
-                <button
-                  onClick={() => handleTypeClick(type)}
-                  className={`px-4 py-2 rounded-lg text-sm border-[1px] border-red-700  ${
-                    isTypeSelected(type)
-                      ? "bg-red-700 text-white hover:bg-red-800 active:bg-red-900 "
-                      : "bg-white text-red-700 hover:bg-slate-100 active:bg-slate-200"
-                  }`}
-                  key={index}
-                >
-                  {type}
-                </button>
-              ))}
+              <div className="flex flex-wrap gap-2">
+                {wineTypes.map((type, index) =>
+                  index <= 5 ? (
+                    <button
+                      onClick={() => handleTypeClick(type)}
+                      className={`px-4 py-2 rounded-lg text-sm border-[1px] border-red-700  ${
+                        isTypeSelected(type)
+                          ? "bg-red-700 text-white hover:bg-red-800 active:bg-red-900 "
+                          : "bg-white text-red-700 hover:bg-slate-100 active:bg-slate-200"
+                      }`}
+                      key={index}
+                    >
+                      {type}
+                    </button>
+                  ) : (
+                    wineTypes.length > 5 &&
+                    showMoreTypes && (
+                      <button
+                        onClick={() => handleTypeClick(type)} // Corrected the onClick function call
+                        id="more-types"
+                        className={`px-4 py-2 rounded-lg text-sm border-[1px] border-red-700  ${
+                          isTypeSelected(type)
+                            ? "bg-red-700 text-white hover:bg-red-800 active:bg-red-900 "
+                            : "bg-white text-red-700 hover:bg-slate-100 active:bg-slate-200"
+                        }`}
+                        key={index}
+                      >
+                        {/* Displaying ellipsis here */}
+                        {type}
+                      </button>
+                    )
+                  )
+                )}
+                {showMoreTypes && (
+                  <button
+                    onClick={handleShowMoreTypesClick} // Corrected the onClick function call
+                    className="px-4 py-2 rounded-lg text-sm border-[1px] border-red-700  bg-slate-100 text-red-700 hover:bg-slate-100 active:bg-slate-200"
+                  >
+                    {showMoreTypes ? "« Show Less " : "Show more »"}
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
-          <div>
-            <h3 className="font-bold text-base mb-6">Grape Types</h3>
-            <div className="flex flex-wrap gap-2">
-              {grapes.map((grape, index) => (
-                <button
-                  onClick={() => handleGrapeClick(grape)}
-                  className={`px-4 py-2 rounded-lg text-sm border-[1px] border-red-700  ${
-                    isGrapeSelected(grape)
-                      ? "bg-red-700 text-white hover:bg-red-800  active:bg-red-900 "
-                      : "bg-white text-red-700 hover:bg-slate-100 active:bg-slate-200"
-                  }`}
-                  key={index}
-                >
-                  {grape}
-                </button>
-              ))}
+          ) : (
+            <></>
+          )}
+          {wineGrapes.length ? (
+            <div>
+              <h3 className="font-bold text-base mb-6">Grape Types</h3>
+
+              <input
+                className="mb-4 file:bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-4 py-2"
+                type="text"
+                placeholder="Search ..."
+              />
+              <div className="flex flex-wrap gap-2">
+                {wineGrapes.map((grape, index) =>
+                  index <= 5 ? (
+                    <button
+                      onClick={() => handleGrapeClick(grape)}
+                      id="more-grapes"
+                      className={`px-4 py-2 rounded-lg text-sm border-[1px] border-red-700  ${
+                        isGrapeSelected(grape)
+                          ? "bg-red-700 text-white hover:bg-red-800 active:bg-red-900 "
+                          : "bg-white text-red-700 hover:bg-slate-100 active:bg-slate-200"
+                      }`}
+                      key={index}
+                    >
+                      {grape}
+                    </button>
+                  ) : (
+                    wineGrapes.length > 5 &&
+                    showMoreGrapes && (
+                      <button
+                        onClick={() => handleGrapeClick(grape)} // Corrected the onClick function call
+                        className={`px-4 py-2 rounded-lg text-sm border-[1px] border-red-700  ${
+                          isGrapeSelected(grape)
+                            ? "bg-red-700 text-white hover:bg-red-800 active:bg-red-900 "
+                            : "bg-white text-red-700 hover:bg-slate-100 active:bg-slate-200"
+                        }`}
+                        key={index}
+                      >
+                        {/* Displaying ellipsis here */}
+                        {grape}
+                      </button>
+                    )
+                  )
+                )}
+                {wineGrapes.length > 5 && (
+                  <button
+                    onClick={handleShowMoreGrapesClick} // Corrected the onClick function call
+                    className="px-4 py-2 rounded-lg text-sm border-[1px] border-red-700  bg-slate-100 text-red-700 hover:bg-slate-100 active:bg-slate-200"
+                  >
+                    {showMoreGrapes ? "« Show Less " : "Show more »"}
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
-          <div>
-            <h3 className="font-bold text-base mb-6">Price Range</h3>
-            <PriceRangeSlider />
-          </div>
+          ) : (
+            <></>
+          )}
         </div>
         <div className="col-span-2 mobile:col-span-1 mobile:gap-6 tablet:col-span-2">
-          {wineList.map((wine) => {
-            return <WineCardVertical key={wine.id} wine={wine} />;
-          })}
-          {/* {displayData(filteredData, category)} */}
+          {wineList.length === 0 ? (
+            <p className="p-6 text-base bg-slate-100 rounded-lg text-slate-400">
+              There are no wines in this category
+            </p>
+          ) : (
+            wineList.map((wine) => {
+              return <WineCardVertical key={wine.id} wine={wine} />;
+            })
+          )}
         </div>
       </div>
     </div>
--- src/utils/data.utils.js
+++ src/utils/data.utils.js
@@ -1,10 +1,11 @@
 import wineData from "../shop-data";
+
 export const getCatagories = () => {
-  const mappedCategories = wineData.map((obj) => {
+  const mappedCategories = wineData.map(({ category, thumbnail, wines }) => {
     return {
-      category: obj.category,
-      thumbnail: obj.thumbnail,
-      count: obj.wines.length,
+      category,
+      thumbnail,
+      count: wines.length,
     };
   });
   return mappedCategories;
@@ -18,14 +19,108 @@ export const getAllWines = (category = null) => {
   } else {
     return wineData
       .flatMap(({ category: cat, wines }) =>
-        wines.map((wine) => {
-          if (cat === category) {
-            return { ...wine, category: cat };
-          } else {
-            return null;
-          }
-        })
+        cat === category
+          ? wines.map((wine) => ({ ...wine, category: cat }))
+          : []
       )
       .filter(Boolean);
   }
+};
+
+// // Filter wine by wine types
+// export const filterWines = (selectedTypes, selectedGrapes) => {
+//   // If no filters are applied, return an empty array
+//   if (selectedTypes.length === 0 && selectedGrapes.length === 0) {
+//     return [];
+//   }
+
+//   let filteredWines = wineData;
+
+//   // Filter wines based on the selected types
+//   if (selectedTypes.length > 0) {
+//     filteredWines = filteredWines
+//       .filter(({ category }) => selectedTypes.includes(category))
+//       .flatMap(({ wines }) => wines);
+//   }
+
+//   // Filter wines based on the selected grapes
+//   if (selectedGrapes.length > 0) {
+//     filteredWines = filteredWines.flatMap(({ wines }) =>
+//       selectedGrapes.every((selectedGrape) =>
+//         wines.filter((wine) => wine.grape.includes(selectedGrape))
+//       )
+//     );
+//   }
+
+//   console.log(filteredWines);
+
+//   // Add the category property to each filtered wine object
+//   const winesWithCategory = filteredWines.map((wine) => ({
+//     ...wine,
+//     category:
+//       wineData.find((data) => data.wines.includes(wine))?.category || "",
+//   }));
+//   // Make sure each wine object has the 'rating' property defined
+//   const winesWithRating = winesWithCategory.map((wine) => ({
+//     ...wine,
+//     rating: wine.rating || 0, // Provide a default rating if 'rating' is undefined
+//   }));
+
+//   return winesWithRating;
+// };
+
+// Filter wine by wine types
+export const filterWines = (selectedTypes, selectedGrapes) => {
+  // If no filters are applied, return an empty array
+  if (selectedTypes.length === 0 && selectedGrapes.length === 0) {
+    return [];
+  }
+
+  const uniqueWineIds = new Set(); // To keep track of unique wine IDs
+  const filteredWines = wineData.reduce((result, categoryData) => {
+    // Filter wines based on the selected types
+    if (
+      selectedTypes.length > 0 &&
+      selectedTypes.includes(categoryData.category)
+    ) {
+      categoryData.wines.forEach((wine) => {
+        if (!uniqueWineIds.has(wine.id)) {
+          result.push(wine);
+          uniqueWineIds.add(wine.id);
+        }
+      });
+    }
+
+    // Filter wines based on the selected grapes
+    if (selectedGrapes.length > 0) {
+      const winesWithSelectedGrapes = categoryData.wines.filter((wine) =>
+        selectedGrapes.some((selectedGrape) =>
+          wine.grape.includes(selectedGrape)
+        )
+      );
+      winesWithSelectedGrapes.forEach((wine) => {
+        if (!uniqueWineIds.has(wine.id)) {
+          result.push(wine);
+          uniqueWineIds.add(wine.id);
+        }
+      });
+    }
+
+    return result;
+  }, []);
+
+  // Add the category property to each filtered wine object
+  const winesWithCategory = filteredWines.map((wine) => ({
+    ...wine,
+    category:
+      wineData.find((data) => data.wines.includes(wine))?.category || "",
+  }));
+
+  // Make sure each wine object has the 'rating' property defined
+  const winesWithRating = winesWithCategory.map((wine) => ({
+    ...wine,
+    rating: wine.rating || 0, // Provide a default rating if 'rating' is undefined
+  }));
+
+  return winesWithRating;
 };
